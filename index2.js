841fca4e8eb14c2446b75e0b71f232a583c62f96
jessica-dds <jessica.gleizer@hotmail.com>
2024-04-15 20:57:42 -0300

    Atualização

--- index2.js
+++ index2.js
@@ -7,13 +7,19 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/:id', async (req, res) => {
+    5
+    const { id } = req.params
+
     try {
-        const resultado = await pool.query('select * from empresas');
-        return res.json(resultado);
+        const query = 'select * from empresas where nome = $1 or nome = $2';
+        const params = ['Google', 'Facebook'];
+
+        const resultado = await pool.query(query, params);
+        return res.json(resultado.rows);
     } catch (error) {
         console.log(error.message);
     }
 });
 
-app.listen(3000)
+app.listen(3000);
