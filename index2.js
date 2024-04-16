e75aed5c1f318650e6f03c71291e24f193623673
jessica-dds <jessica.gleizer@hotmail.com>
2024-04-15 21:20:25 -0300

    Atualização

--- index2.js
+++ index2.js
@@ -12,8 +12,11 @@ app.get('/:id', async (req, res) => {
     const { id } = req.params
 
     try {
-        const query = 'select * from empresas where nome = $1 or nome = $2';
-        const params = ['Google', 'Facebook'];
+        // const query = 'select * from empresas where nome = $1 or nome = $2';
+        // const params = ['Google', 'Facebook'];
+
+        const query = 'update empresas set site = $1 where id = $2';
+        const params = ['www.cakewalk.com', 1];
 
         const resultado = await pool.query(query, params);
         return res.json(resultado.rows);
