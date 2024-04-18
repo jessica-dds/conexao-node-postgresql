7fb35a124b5bd14cf59f6cf6dce10c764fa83f2f
jessica-dds <jessica.gleizer@hotmail.com>
2024-04-17 22:20:32 -0300

    final

--- index2.js
+++ index2.js
@@ -11,22 +11,54 @@ app.get('/', async (req, res) => {
 
     //const { id } = req.params
 
+    const { pagina, porPagina } = req.query;
+
     try {
+
         // const query = 'select * from empresas where nome = $1 or nome = $2';
         // const params = ['Google', 'Facebook'];
 
         // const query = 'update empresas set site = $1 where id = $2';
         // const params = ['www.cakewalk.com', 1];
 
-        const query = `
-        select e.id as empresaId, f.id as filialId, e.nome, f.pais, p.nome as funcionario
-        from empresas e 
-        join filiais f on e.id = f.empresa_id
-        join pessoas p on e.id = p.empresa_id;
-        `
+        // const query = `
+        // select e.id as empresaId, f.id as filialId, e.nome, f.pais, p.nome as funcionario
+        // from empresas e 
+        // join filiais f on e.id = f.empresa_id
+        // join pessoas p on e.id = p.empresa_id;
+        // `
 
-        const resultado = await pool.query(query);
-        return res.json(resultado.rows); //rowCount = quantidade de registros - rows = tudo que houver no banco de dados
+        // const query = `
+        // select e.id as empresaId, f.id as filialId, e.nome, f.pais
+        // from empresas e 
+        // left join filiais f on e.id = f.empresa_id;
+        // `
+
+        // const query = `
+        // select e.id as empresaId, f.id as filialId, e.nome, f.pais
+        // from empresas e 
+        // right join filiais f on e.id = f.empresa_id;
+        // `
+
+
+        // const query = `
+        // select e.id as empresaId, f.id as filialId, e.nome, f.pais
+        // from empresas e 
+        // full join filiais f on e.id = f.empresa_id;
+        // `
+
+        const query = 'select * from pessoas order by id asc limit $1 offset $2';
+
+        const offset = pagina == 1 ? 0 : (pagina - 1) * porPagina;
+
+        const resultado = await pool.query(query, [porPagina, offset]); // muda conforme os params 
+        const result = {
+            pagina,
+            porPagina,
+            total: resultado.rowCount,
+            registros: resultado.rows
+        };
+        return res.json(result); //rowCount = quantidade de registros - rows = tudo que houver no banco de dados
     } catch (error) {
         console.log(error.message);
     }
